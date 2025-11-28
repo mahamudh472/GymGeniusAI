5090b54a9548aec0ea0148ade49672929ef4ee37
Md Mahmud Hasan <mahadymahamudh472@gmail.com>
2025-11-28 11:15:40 +0600

    contact option added

--- GymGeniusAI/settings.py
+++ GymGeniusAI/settings.py
@@ -245,3 +245,4 @@ SPECTACULAR_SETTINGS = {
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
+CSRF_TRUSTED_ORIGINS = ['http://localhost:8000', 'http://127.0.0.1:8000', 'https://sisterlike-tastelessly-mike.ngrok-free.dev/']
